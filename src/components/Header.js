e09fb043349f555ad68730e55d7efeb6726a731a
puja-pandey <pujakghs79@gmail.com>
2025-04-06 19:40:16 +0530

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+    const navigate = useNavigate();
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+        navigate('/login');
+    }
+return(
+    <header className="topNav">
+        <nav className="navbar navbar-expand-md navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img className="nav_logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"></img>
+                </a>
+                <div className="navbar">
+                    <from className="d-flex" role="search">
+                        <select>
+                            <option>English</option>
+                            <option>Hindi</option>
+                        </select>
+                        <button className="btn btn-danger" onClick={clickHandler}>Sign-in</button>
+                    </from>
+                </div>
+            </div>
+        </nav>
+    </header>
+)
+}
+
+export default Header;
---
+++ src/components/List.js
@@ -0,0 +1,45 @@
+const List = () => {
+    return(
+      <div className="list">
+        <div className="row">
+            <h2 className="text-white title">Netflix</h2>
+            <div className="col">
+                <div className="row__posters">
+                    <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+                     
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                     <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+
+                      <img className="row__poster row__posterLarge"
+                    src="https://m.media-amazon.com/images/M/MV5BODM3NjdkN2UtOTFiZi00Zjg2LWIwYWMtZGQ0YjZjNWFjYjUxXkEyXkFqcGc@._V1_QL75_UY281_CR18,0,190,281_.jpg"
+                    alt="Jupiters LEGACY"/>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+}
+export default List;
