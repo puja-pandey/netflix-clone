dd116bf1101a89db361c6849174b5afc06f8e1a0
puja-pandey <pujakghs79@gmail.com>
2025-04-06 20:51:07 +0530

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,37 @@
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const clickHandler = (e) => {
-        e.preventDefault();
-        navigate('/login');
-    }
-return(
+  const clickHandler = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+  return (
     <header className="topNav">
-        <nav className="navbar navbar-expand-md navbar-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img className="nav_logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"></img>
-                </a>
-                <div className="navbar">
-                    <from className="d-flex" role="search">
-                        <select>
-                            <option>English</option>
-                            <option>Hindi</option>
-                        </select>
-                        <button className="btn btn-danger" onClick={clickHandler}>Sign-in</button>
-                    </from>
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar navbar-expand-md navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/netflix-clone">
+            <img
+              className="nav_logo"
+              src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+            ></img>
+          </a>
+          <div className="navbar">
+            <from className="d-flex" role="search">
+              <select>
+                <option>English</option>
+                <option>Hindi</option>
+              </select>
+              <button className="btn btn-danger" onClick={clickHandler}>
+                Sign-in
+              </button>
+            </from>
+          </div>
+        </div>
+      </nav>
     </header>
-)
-}
+  );
+};
 
 export default Header;
